bb00f1bc5c4dae1b0ff8d442bad38948f1b724df
Ralph van Schaik <ralph.van.schaik@me.com>
2020-10-25 11:05:23 +0100

    Tm 88 bonus life

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,12 +5,22 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound(){
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+
+  if(currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    alert("You would have been dead but your bonus life saved your ass!")
+    setPlayerHealth(initialPlayerHealth);
+  }
 
   if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!');
